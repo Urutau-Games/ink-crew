548bcdbb2172c6fe32dbd060737fd2ecb27845d1
Lázaro Menezes <lazarodm@gmail.com>
2025-11-10 21:44:59 -0300

    Initialized bomb explosion raycast

--- bomb.gd
+++ bomb.gd
@@ -3,11 +3,16 @@ extends Node3D
 signal exploded()
 
 @export var bomb_timer := 1.5
-
+@export var bomb_strength := 1
 
 func _ready() -> void:
-	get_tree().create_timer(bomb_timer).timeout.connect(_explode)
+	for raycast: RayCast3D in %RayCasts.get_children():
+		raycast.target_position *= bomb_strength
+		get_tree().create_timer(bomb_timer).timeout.connect(_explode)
 
 func _explode():
+	for raycast: RayCast3D in %RayCasts.get_children():
+		raycast.enabled = true
+		
 	exploded.emit()
 	queue_free()
